e49d106121c34bb6b235545ab5b01e74c66d2fa5
Carlos W. Gama <carloswgama@gmail.com>
2018-06-21 09:33:04 -0300

    relogio e tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild("clock")
+  clock;
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad() {
+    
+    
+    var tempo = [];
+    var cores = [];
+    
+
+    tempo = [3, 4.5, 0.2, 4];
+    cores = [
+      'rgba(86, 206, 86, 0.5)',
+      'rgba(255, 255, 255, 0)',
+      'rgba(86, 206, 86, 0.5)',
+      'rgba(255, 255, 255, 0)'
+    ];
+
+    this.clock = new Chart(this.clock.nativeElement, {
+      type: 'doughnut',
+      data: {
+          datasets: [{
+              data: tempo,
+              backgroundColor: cores
+          }]
+      },
+      options: {
+        tooltips: {enabled: false},
+        animation: {animateRotate: false},
+        rotation: -1.05 * 0.5 * (3),
+        cutoutPercentage: 0,
+        legend: {
+          display: false
+        }
+      }
+
+  });
 
   }
 
